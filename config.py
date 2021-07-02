9dbefdafcf175974698ea888ebea22cb12c4da47
Tyler <twwhitehurst@gmail.com>
2021-07-01 23:24:04 -0400

    Add gif

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 MAX_ROWS = 8
 MAX_COLUMNS = 20
 MARGIN = 16
-TITLE_BAR_OFFSET_Y = 0
+TITLE_BAR_OFFSET_Y = -32
 ABSOLUTE_PATH_TO_PYTHON = "/usr/bin/python3"
 ABSOLUTE_PATH_TO_SHUFFLER = "/home/tyler/projects/window-shuffler/window_shuffler"
 ABSOLUTE_PATH_TO_SHUFFLER_NO_GUI = "/home/tyler/projects/window-shuffler/window_shuffler_no_gui.py"
